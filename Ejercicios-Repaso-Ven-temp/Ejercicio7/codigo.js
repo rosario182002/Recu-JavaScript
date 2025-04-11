9a519464b959828087479534309f1dd1d8353783
rosario182002 <rosariodelgadomoreno17@gmail.com>
2025-04-11 16:50:51 +0200

    hoy

---
+++ Ejercicios-Repaso-Ven-temp/Ejercicio7/codigo.js
@@ -0,0 +1,10 @@
+var nombre = document.getElementById("nombre").value;
+var java = document.getElementById("java").value;
+var cliente = document.getElementById("cliente").value;
+var php = document.getElementById("php").value;
+
+
+document.getElementById("boton")
+if(nombre == "" || java =="" || cliente == "" || php == ""){
+        alert("Todos los campos deben ser completados");
+}
