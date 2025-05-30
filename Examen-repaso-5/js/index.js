935bd1a22cb9411135eee08e73917b0b945611ce
rosario182002 <rosariodelgadomoreno17@gmail.com>
2025-05-30 16:03:12 +0200

    30/05/2025

--- Examen-repaso-5/js/index.js
+++ Examen-repaso-5/js/index.js
@@ -38,18 +38,77 @@ function recomendar(){
         //Generar un numero aleatorio entre 0 y 4
         const indiceAleatorio = Math.floor(Math.random() * peliculas.length);
         //Obtenemos la pelicula seleccionada
+       
         const peliculaSeleccionada = peliculas[indiceAleatorio];
-        
+       
         //Mostrar el mensaje de recomendacion
-       if(indiceAleatorio === peliculas.length - 1){
-        peliculaSeleccionada.innerHTML =`<h2>Oferta del dia: ${peliculaSeleccionada}</h2>`;
+       if(indiceAleatorio === 0){
+        peliculasDiv.innerHTML =`<h2>Oferta del dia: ${peliculaSeleccionada}</h2>`;
        }
        else{    
-        peliculaSeleccionada.innerHTML = `<h2>Ofertón del día: ${peliculaSeleccionada}</h2>`;
+        peliculasDiv.innerHTML = `<h2>Ofertón del día: ${peliculaSeleccionada}</h2>`;
        }
     }, 5000);
 }
     //Asignamos la funcion al boton
-    const botonRecomendar = document.getElementById("recomendar");
+    const botonRecomendar = document.getElementById("recomendar");                             
     botonRecomendar.addEventListener("click", recomendar);
+
+//D) funcionalidad de Mostrar Peliculas
+
+function mostrarPeliculas(){
+    const pelicula = document.getElementById("resultado");
+    //Crear la tabla
+    let tablaHTML = "<table border='1' >";
+
+    //Agregamos las filas con los titulos
+    peliculas.forEach((titulo, index) => {
+        tablaHTML += `
+        <tr> 
+            <td class="titulo">${index+1}. ${titulo}</td>
+            <td><input type="text" value="0" class="cuadro"></td>
+        </tr>
+        `;
+    });
+    //Cerrar la tabla
+    tablaHTML += "</table>";
+    //Insertar la tabla en el div
+    pelicula.innerHTML = tablaHTML;
+
+    //Habilitar el boton de alquilar
+    const botonAlquilar = document.getElementById("alquilar");
+    botonAlquilar.disabled = false;
+} 
+
+//Asignar la funcion al boton con id="mostrarPeliculas"
+const botonMostrarPeliculas = document.getElementById("mostrarPeliculas");
+botonMostrarPeliculas.addEventListener("click", mostrarPeliculas);
+
+
+
+//E) Funcionalidad de alquilar peliculas
+function alquilar(){
+    //obtenemos TODOS los inputs de la tabla
+    const inputs = Array.from(document.getElementsByClassName("cuadro"));
+    let haypelis = false;
+
+    //Verificamos si hay alguna pelicula seleccionada
+    inputs.forEach(inputs =>{
+        if(parseInt(inputs.value)>0){
+            haypelis = true;
+        }
+    });
+    if(!haypelis){
+        //Mostramos la alerta si no hay libros seleccionados
+        alert("No hay ningún libro que comprar");
+        return;
+    }
+    //F ABRIMOS LA NUEVA VENTANA QUE ES RESUMEN.JS
+    window.open("resumen.html", "Resume", "witdh=600,height=500");
+}
+
+botonAlquilar.addEventListener("click", alquilar);
+
+
 });
+
